8f5e094101fdd982037cf06c80e1692d76a14a5e
Viktor von Westarp <viktor.grafvonwestarp@student.hs-anhalt.de>
2024-10-05 14:34:44 +0200

    dont build exe if the game is running

--- build.bat
+++ build.bat
@@ -4,11 +4,16 @@ set build-flags=-warnings-as-errors -vet-cast -vet-shadowing -error-pos-style:un
 set debug-flags=-debug -o:none
 
 IF NOT EXIST .\build mkdir .\build
-REM TODO shipping build
+:: TODO shipping build
 
-REM debug build
+:: Debug build
 del .\build\*.pdb
 odin build game -build-mode:dll -out:.\build\game.dll %build-flags% %debug-flags% -pdb-name:.\build\game-%random%.pdb
-odin build . -out:.\build\debug.exe %build-flags% %debug-flags% -define:INTERNAL=true
+
+:: if the game is already running exit early
+set EXE=debug.exe
+for /f %%x in ('tasklist /NH /FI "IMAGENAME eq %EXE%"') do if %%x == %EXE% exit /b 0
+
+odin build . -out:.\build\%EXE% %build-flags% %debug-flags% -define:INTERNAL=true
 
 exit /b 0
